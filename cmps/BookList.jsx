596e5857d9d90bae653cea40fc8f192af65f4247
ReactRay <ray.diaz1899@gmail.com>
2024-11-25 15:56:14 +0200

    Your commit message here

---
+++ cmps/BookList.jsx
@@ -0,0 +1,18 @@
+import { BookPreview } from './BookPreview.jsx'
+
+const { Link } = ReactRouterDOM
+
+export function BookList({ books, removeBook }) {
+  return (
+    <div >
+      <div className="book-flex">
+        {books.map((book, idx) => (
+          <div key={book.id} className="book-item">
+            <BookPreview book={book} removeBook={removeBook} imgNum={idx} />
+
+          </div>
+        ))}
+      </div>
+    </div>
+  )
+}
---
+++ cmps/BookPreview.jsx
@@ -0,0 +1,24 @@
+import { bookService } from '../services/book.service.js'
+const { Link } = ReactRouterDOM
+
+export function BookPreview({ book, removeBook, imgNum }) {
+  return (
+    <div className="book-box">
+      <h2>Name: {book.bookName}</h2>
+      <h3>Desc: {book.bookDesc}</h3>
+      <h3>
+        <span className="price">Price: {book.bookPrice}$</span>
+      </h3>
+      <img src={`../assets/BooksImages/${imgNum + 1}.jpg`} />
+      <div className="btn-box">
+
+        <Link className='nav' to={`/book/bookdetails/${book.id}`}>select book</Link>
+
+        <button className='btn' onClick={() => removeBook(book.id)}>Remove Book</button>
+
+        <Link className='nav' to={`/book/add/${book.id}`}>edit Book</Link> (Todo)
+
+      </div>
+    </div>
+  )
+}
